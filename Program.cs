14dda9333a1be92a7c6ca9216e1d05e8853ecd6a
Roger Dominguez <rogerd@mail.usf.edu>
2020-02-06 13:51:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            Console.Write("INPUT: ");
+            name = Console.ReadLine();
+            Console.WriteLine("OUTPUT: Hello {0}!", name);
+
+        }
+    }
+}
