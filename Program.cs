3386275705cf4448f9bd61ecdc3f83d3f82f512c
Roger Dominguez <rogerd@mail.usf.edu>
2020-02-06 13:53:26 -0500

    commit changes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//Roger Dominguez
+// 2/6/2020
+// Using a method to display input and output of user name
+using System;
 
 namespace Methods
 {
@@ -6,9 +9,13 @@ namespace Methods
     {
         static void Main(string[] args)
         {
+            // Declare name a string
             string name;
+            //Ask User for an input
             Console.Write("INPUT: ");
+            // Read User input
             name = Console.ReadLine();
+            // Display output based on user input
             Console.WriteLine("OUTPUT: Hello {0}!", name);
 
         }
